dd49dbf8684e01b4efffaba5aa4f1fbb660a532a
Saryn Mooney <smooney15@my.whitworth.edu>
2020-06-05 12:09:37 -0600

    possible key random generation approach

--- lib/key.rb
+++ lib/key.rb
@@ -1,6 +1,25 @@
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-numbers.sample(5)
+class Key
+  attr_reader :numbers
 
-numbers = (0..9)
+  def initialize
+    @numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
 
-10.times.map{rand(10)}.join # => "3401487670"
+  def randomize
+    key = 5.times.map{rand(5)}.join
+    p key 
+  end
+
+  r = Key.new
+  r.randomize
+  # random = Key.new
+  # p random.numbers
+
+end
+
+
+# numbers.sample(5)
+#
+#
+#
+# 10.times.map{rand(10)}.join # => "3401487670"
